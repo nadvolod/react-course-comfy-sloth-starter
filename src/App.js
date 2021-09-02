49bdd601471b1c1937d3cc96c495da2b5067b5e1
nadvolod <nadvolod@gmail.com>
2021-09-02 18:32:47 -0400

    reset to start

--- src/App.js
+++ src/App.js
@@ -1,13 +1,12 @@
 import React from 'react'
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom'
 import { Navbar, Sidebar, Footer } from './components'
-import Testing from './Testing'
 
 
 function App() {
   return (
     <div>
-      <Testing></Testing>
+      <h1>Comfy Sloth</h1>
     </div>
   )
 }
