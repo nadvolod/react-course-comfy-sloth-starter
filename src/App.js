6132d717159cad3ff8d259057d8d5cfefa415923
nadvolod <nadvolod@gmail.com>
2021-09-02 18:59:09 -0400

    add all pages to app.js

--- src/App.js
+++ src/App.js
@@ -1,13 +1,40 @@
 import React from 'react'
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom'
+//This is a cleaner way to import multiple objects from a folder
+//instead of importing one object at a time from a file
+//just make sure to have an index.js
 import { Navbar, Sidebar, Footer } from './components'
-
+import { Home, Products, SingleProduct, About, Cart, Error, PrivateRoute, CheckoutPage } from './pages'
 
 function App() {
   return (
-    <div>
-      <h1>Comfy Sloth</h1>
-    </div>
+    <Router>
+      <Navbar></Navbar>
+      <Sidebar></Sidebar>
+      <Switch>
+        <Route exact path='/'>
+          <Home></Home>
+        </Route>
+        <Route exact path='/about'>
+          <About />
+        </Route>
+        <Route exact path='/cart'>
+          <Cart></Cart>
+        </Route>
+        <Route exact path='/products'>
+          <Products></Products>
+        </Route>
+        <Route exact path='/products/:id' children={<SingleProduct />}>
+        </Route>
+        <Route exact path='/checkout' >
+          <CheckoutPage></CheckoutPage>
+        </Route>
+        <Route path='*' >
+          <Error></Error>
+        </Route>
+      </Switch>
+      <Footer></Footer>
+    </Router>
   )
 }
 
---
+++ src/pages/index.js
@@ -0,0 +1,11 @@
+import Home from './HomePage'
+import Products from './ProductsPage'
+import SingleProduct from './SingleProductPage'
+import About from './AboutPage'
+import Cart from './CartPage'
+import Error from './ErrorPage'
+import PrivateRoute from './PrivateRoute'
+import CheckoutPage from './CheckoutPage'
+
+//exporting as a named export
+export { Home, Products, SingleProduct, About, Cart, Error, PrivateRoute, CheckoutPage }
